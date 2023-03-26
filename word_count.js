ad59db69c70baa43b0c4b324ed67c4a9abeae38d
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-03-26 16:57:48 +0200

    formato

--- word_count.js
+++ word_count.js
@@ -1,16 +1,16 @@
 // El módulo fs se utiliza para leer y escribir archivos. Para usarlo, debe solicitarlo en la parte superior de su archivo:
 const fs = require("fs");
- const read = fs.readFileSync('sample.txt',"utf-8");
+const read = fs.readFileSync('sample.txt', "utf-8");
 // 1º Eliminar de sample.txt signos de puntuación, mayusculas y convertirlo en array con split. 
- const content = read.toLocaleLowerCase().replaceAll(".","").replaceAll(",","").replaceAll("-","").split(" ");
+const content = read.toLocaleLowerCase().replaceAll(".", "").replaceAll(",", "").replaceAll("-", "").split(" ");
 // 2º Variable contador para la función forEach.
 let javascriptCount = 0;
-//3º Funcion forEach para recorrer el array creado con sample.txt e ir sumando en la variable anterior cuantas veces está la palabra "javascript"
+// 3º Funcion forEach para recorrer el array creado con sample.txt e ir sumando en la variable anterior cuantas veces está la palabra "javascript"
 content.forEach(element => {
-    if(element === "javascript"){
-         javascriptCount ++
+    if (element === "javascript") {
+        javascriptCount++
     }
 });
 //Crear nuevo archivo llamado count.txt con el metodo de node writeFileSync , y volcar la cadena de texto que se indica en el segundo parámetro concatenando la variable contador. 
-const write = fs.writeFileSync('count.txt',"The word 'JavaScript' appears "+ javascriptCount+ " times in the text.")
+const write = fs.writeFileSync('count.txt', "The word 'JavaScript' appears " + javascriptCount + " times in the text.")
 console.log(write)
